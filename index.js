59ec7b6fcd125564afc1d81e5e33322d66e0c7b9
higorcostaeear-commits <matheus.lopes@sondotecnica.com.br>
2026-02-02 17:05:19 -0300

    Simplify Vercel configuration - working setup

--- index.js
+++
@@ -1,6 +0,0 @@
-// Este arquivo serve apenas como entrypoint para a Vercel
-// As funções serverless estão em /api
-
-module.exports = (req, res) => {
-  res.status(404).json({ message: 'Not found' });
-};
